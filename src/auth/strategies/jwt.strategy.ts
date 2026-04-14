655cdc497907e12ec3cc30e1b7a7a4505f9cadfe
Harold <harold.apaza.monasterio@gmail.com>
2026-04-13 20:14:52 -0400

    feat : create user role and login..

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,4 +1,4 @@
-﻿import { Injectable, UnauthorizedException } from '@nestjs/common';
+﻿import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayload } from '../interfaces/jwt-payload.interface';
@@ -13,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: JwtPayload): Promise<any> {
+  validate(payload: JwtPayload): any {
     return {
       id: payload.sub,
       username: payload.username,
