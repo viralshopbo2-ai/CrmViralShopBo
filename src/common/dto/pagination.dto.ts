92a46590035e7cbe69ec56090ea6a582526f3dbf
Harold <harold.apaza.monasterio@gmail.com>
2026-04-21 00:55:43 -0400

    feature: ended login with roles 2

---
+++ src/common/dto/pagination.dto.ts
@@ -0,0 +1,17 @@
+﻿import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsOptional, IsPositive, Min } from 'class-validator';
+
+export class PaginationDto {
+  @ApiProperty({ example: 1, required: false })
+  @IsOptional()
+  @IsPositive()
+  @Type(() => Number)
+  page?: number = 1;
+
+  @ApiProperty({ example: 10, required: false })
+  @IsOptional()
+  @IsPositive()
+  @Type(() => Number)
+  size?: number = 10;
+}
